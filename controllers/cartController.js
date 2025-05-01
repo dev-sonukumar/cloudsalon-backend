498b6241842b9070821a8cc51f3ffdb0923b9163
sonu kumar <sonu638056@gmail.com>
2025-05-01 13:13:33 +0530

    update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,75 +1,78 @@
-import userModel from "../models/userModel.js"
-
+import userModel from "../models/userModel.js";
 
 // add products to user cart
-const addToCart = async (req,res) => {
-    try {
-        
-        const { userId, itemId, size } = req.body
+// Add products to user cart (no size variant)
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId } = req.body;
 
-        const userData = await userModel.findById(userId)
-        let cartData = await userData.cartData;
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData || {};
 
-        if (cartData[itemId]) {
-            if (cartData[itemId][size]) {
-                cartData[itemId][size] += 1
-            }
-            else {
-                cartData[itemId][size] = 1
-            }
-        } else {
-            cartData[itemId] = {}
-            cartData[itemId][size] = 1
-        }
-
-        await userModel.findByIdAndUpdate(userId, {cartData})
-
-        res.json({ success: true, message: "Added To Cart" })
-
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
+    // If item already exists, increase quantity
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
     }
-}
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+
+    res.json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const mergeCart = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { mergedCart } = req.body;
+
+    const user = await User.findById(userId);
+    user.cart = mergedCart;
+    await user.save();
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Merge failed", error: err.message });
+  }
+};
 
 // update user cart
-const updateCart = async (req,res) => {
-    try {
-        
-        const { userId ,itemId, size, quantity } = req.body
+const updateCart = async (req, res) => {
+  try {
+    const { userId, itemId, size, quantity } = req.body;
 
-        const userData = await userModel.findById(userId)
-        let cartData = await userData.cartData;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
 
-        cartData[itemId][size] = quantity
+    cartData[itemId][size] = quantity;
 
-        await userModel.findByIdAndUpdate(userId, {cartData})
-        res.json({ success: true, message: "Cart Updated" })
-
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
-
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Cart Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // get user cart data
-const getUserCart = async (req,res) => {
+const getUserCart = async (req, res) => {
+  try {
+    const { userId } = req.body;
 
-    try {
-        
-        const { userId } = req.body
-        
-        const userData = await userModel.findById(userId)
-        let cartData = await userData.cartData;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
 
-        res.json({ success: true, cartData })
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-
-}
-
-export { addToCart, updateCart, getUserCart }
+export { addToCart, updateCart, getUserCart, mergeCart };
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,11 +1,17 @@
-import express from 'express'
-import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js'
-import authUser from '../middleware/auth.js'
+import express from "express";
+import {
+  addToCart,
+  getUserCart,
+  updateCart,
+  mergeCart,
+} from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
 
-const cartRouter = express.Router()
+const cartRouter = express.Router();
 
-cartRouter.post('/get',authUser, getUserCart)
-cartRouter.post('/add',authUser, addToCart)
-cartRouter.post('/update',authUser, updateCart)
+cartRouter.post("/get", authUser, getUserCart);
+cartRouter.post("/merge", authUser, mergeCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
 
-export default cartRouter
+export default cartRouter;
